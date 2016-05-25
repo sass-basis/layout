5fb8db26d1abe65f3ff0f43da81e29414400ac70
Takashi Kitajima <inc@2inc.org>
2016-05-25 23:27:07 +0900

    some fixes

--- src/js/fixed-header.js
+++ src/js/fixed-header.js
@@ -1,46 +1,62 @@
 'use strict';
 
 export default class BasisFixedHeader {
-	constructor(container, params) {
-		if (!container) {
-			container = '._l-container';
-		}
-		if (!params) {
-			params = {};
-		}
-		this.params = params;
-		if (!this.params.header) {
-			this.params.header = '._l-header';
-		}
-		if (!this.params.class) {
-			this.params.class = '_l-header--is-scrolled';
-		}
+  constructor(container, params) {
+    if (!container) {
+      container = '._l-container';
+    }
+    if (!params) {
+      params = {};
+    }
+    this.params = params;
+    if (!this.params.header) {
+      this.params.header = '._l-header';
+    }
+    if (!this.params.class) {
+      this.params.class = '_l-header--is-scrolled';
+    }
 
-		this.container = document.querySelector(container);
-		this.header    = document.querySelector(this.params.header);
-		this.setHeaderWidth();
-		this.setListener();
-	}
+    this.container = document.querySelector(container);
+    this.header    = document.querySelector(this.params.header);
+    this.setHeaderWidth();
+    this.setListener();
+  }
 
-	setListener() {
-		this.container.addEventListener('scroll', (event) => {
-			const scroll = container.scrollTop;
-			if (scroll > 0) {
-				this.header.classList.add(this.params.class);
-			} else {
-				this.header.classList.remove(this.params.class);
-			}
-		}, false);
+  setListener() {
+    const isDisableWindowScroll = document.getElementsByTagName('html')[0].classList.contains('_disable-window-scroll');
+    let target;
+    if (isDisableWindowScroll) {
+      target = this.container;
+    } else {
+      target = window;
+    }
 
-		window.addEventListener('resize', (event) => {
-			this.setHeaderWidth();
-		}, false);
-	}
+    target.addEventListener('scroll', (event) => {
+      let scroll = 0;
+      if (isDisableWindowScroll) {
+        scroll = this.container.scrollTop;
+      } else {
+        scroll = window.pageYOffset;
+      }
+      if (!isDisableWindowScroll) {
+        target = window;
+      }
+      if (scroll > 0) {
+        this.header.classList.add(this.params.class);
+      } else {
+        this.header.classList.remove(this.params.class);
+      }
+    }, false);
 
-	setHeaderWidth() {
-		const scrollbarWidth = document.body.clientWidth - this.container.clientWidth;
-		if (scrollbarWidth > 0) {
-			this.header.style.width = 'calc(100% - ' + scrollbarWidth + 'px)';
-		}
-	}
+    window.addEventListener('resize', (event) => {
+      this.setHeaderWidth();
+    }, false);
+  }
+
+  setHeaderWidth() {
+    const scrollbarWidth = document.body.clientWidth - this.container.clientWidth;
+    if (scrollbarWidth > 0) {
+      this.header.style.width = 'calc(100% - ' + scrollbarWidth + 'px)';
+    }
+  }
 }
